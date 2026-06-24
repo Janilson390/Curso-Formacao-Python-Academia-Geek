d0a66bd6fdea50500838e5ff5d2c723ce020450c
Janilson Florencio da Silva <janilson390@gmail.com>
2026-06-24 17:08:29 -0300

    Aula Recebendo dados do usuário

--- recebenco_dados_usuario.py
+++ recebenco_dados_usuario.py
@@ -1,4 +1,5 @@
-
+# Python 2.x
+"""
 print("Qual o seu nome?")
 nome = input()
 
@@ -6,5 +7,12 @@ print("Seja bem-vindo(a) %s" % nome)
 
 print("\nQuual a sua idade?")
 idade = input()
-
 print("%s! Você tem %s anos!" % (nome, idade))
+"""
+
+# Python 3.x
+nome = input("Qual o seu nome?\n")
+print("Seja bem-vindo(a) {0}".format(nome))
+
+idade = input("\nQual a sua idade?\n")
+print(f"{nome}! Você tem {idade} anos! Você nasceu em {2026 - int(idade)}.")
